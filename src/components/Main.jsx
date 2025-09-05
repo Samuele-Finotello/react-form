d0c39171647adda901d41d0434187b0e7f1c4b27
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-05 18:56:00 +0200

    [add] feat milestone 2

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,64 +1,31 @@
 import { useState } from "react"
 
 const Main = () => {
-  const posts = [
-    {
-      id: 1,
-      title: 'Spiaggia'
-    },
-    {
-      id: 2,
-      title: 'Lago'
-    },
-    {
-      id: 3,
-      title: 'Montagna'
-    },
-    {
-      id: 4,
-      title: 'Mare'
-    },
-    {
-      id: 5,
-      title: 'Francia'
-    },
-    {
-      id: 6,
-      title: 'Grigliata'
-    },
-    {
-      id: 7,
-      title: 'Natale'
-    },
-    {
-      id: 8,
-      title: 'Pasqua'
-    },
-    {
-      id: 9,
-      title: 'Ferragosto'
-    },
-    {
-      id: 10,
-      title: 'Concerto'
-    }
-  ]
+  const initialPosts = ['Spiaggia', 'Lago', 'Montagna', 'Mare', 'Francia', 'Grigliata', 'Natale', 'Pasqua', 'Ferragosto', 'Concerto']
 
-  const [addPost, setAddPost] = useState(null);
+  const [posts, setPosts] = useState(initialPosts)
+  const [addPost, setAddPost] = useState('');
 
   return (
     <div>
-      {posts.map(post => {
-        return (
-          <ul className="list-group" key={post['id']}>
-            <li className="list-group-item fs-4">{post['title']}</li>
-          </ul>
-        )
-      })}
-      <div className="d-flex col-12">
-        <input type="text" className="form-control mt-5 fs-4" placeholder="Inserisci un nuovo post" />
-        <button className="btn btn-success mt-5 fs-4" onClick={() => { setAddPost(addPost) }}>Aggiungi</button>
-      </div>
+      <ul className="list-group">
+        {posts.map(post => {
+          return (
+            <li className="list-group-item fs-4">{post}</li>
+          )
+        })}
+      </ul>
+      <form className="d-flex col-12" onSubmit={(e) => {
+        e.preventDefault()
+        const copyPosts = [...posts, addPost];
+        setPosts(copyPosts)
+      }}>
+        <input type="text" value={addPost} className="form-control mt-5 fs-4" placeholder="Inserisci un nuovo post"
+          onChange={(e) => {
+            setAddPost(e.target.value)
+          }} />
+        <button className="btn btn-success mt-5 fs-4" >Aggiungi</button>
+      </form>
     </div>
   )
 }
